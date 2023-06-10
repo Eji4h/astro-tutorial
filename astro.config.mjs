ac686121dd049fddeb919dd54599084f0ce297e7
Yosapol Jitrak <eji4h@Yosapols-MacBook-Pro.local>
2023-06-11 05:22:40 +0700

    refactor: 💡 add type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-});
+	// your configuration options here...
+	// https://docs.astro.build/en/reference/configuration-reference/
+})
