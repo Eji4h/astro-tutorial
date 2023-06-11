b9d850f28eef46317598dfbe6be58828b6fcc035
Yosapol Jitrak <eji4h@Yosapols-MacBook-Pro.local>
2023-06-12 01:05:24 +0700

    feat: 🎸 add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   head: {
     // ...other head meta tags
     link: [
-      // Add the following line to change the web icon
-      { rel: 'icon', type: 'image/png', href: './src/assets/icons/jitrak-icon.png' },
-    ],
+    // Add the following line to change the web icon
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: './src/assets/icons/jitrak-icon.png'
+    }]
   },
   experimental: {
     assets: true
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()]
 } // your configuration options here...
-  // https://docs.astro.build/en/reference/configuration-reference/
+// https://docs.astro.build/en/reference/configuration-reference/
 );
