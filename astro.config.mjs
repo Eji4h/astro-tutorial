c0bce883573a8fef0bc4ddeb3d337989e0aba662
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2023-06-12 03:50:18 +0700

    feat: 🎸 handle image undefined

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,17 @@ export default defineConfig({
   head: {
     // ...other head meta tags
     link: [
-    // Add the following line to change the web icon
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: './src/assets/icons/jitrak-icon.png'
-    }]
+      // Add the following line to change the web icon
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: './src/assets/icons/jitrak-icon.png'
+      }]
   },
   experimental: {
     assets: true
   },
   integrations: [tailwind(), mdx()]
 } // your configuration options here...
-// https://docs.astro.build/en/reference/configuration-reference/
+  // https://docs.astro.build/en/reference/configuration-reference/
 );
